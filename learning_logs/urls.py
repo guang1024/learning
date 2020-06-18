fd8931563b48233f69f6c95754b1b8d85d8b5506
guang1024 <guangchuan.shi@percent.cn>
2020-06-18 23:49:15 +0800

    test

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+'''定义learning_logs的URL模式'''
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # 主页
+    #错误:path(r'^$', views.index, name='index'),    #不匹配正则
+    path(r'', views.index, name='index'),
+    #显示所有的主题
+    path(r'^topic/$', views.topics, name='topic'),
+]
---
+++ learning_logs/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from .models import Topic
+
+# Create your views here.
+
+
+def index(request):
+    """学习笔记的主页"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    '''显示所有的主题'''
+    topic = Topic.objects.order_by('date_added')
+    context = {'topics': topic}
+    return render(request, 'learning_logs/topics.html', context)
