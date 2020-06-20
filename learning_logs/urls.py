3a458b6ca4391001a8056c5a35d6d0e3d7d0b04d
guang1024 <guangchuan.shi@percent.cn>
2020-06-21 05:56:44 +0800

    test

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
     #新加主题条目
     path(r'new_topic/', views.new_topic, name='new_topic'),
-    re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry')
+    re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
 ]
