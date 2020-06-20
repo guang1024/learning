1c47887417411280f28b70e44b1f73219d1e467c
guang1024 <guangchuan.shi@percent.cn>
2020-06-21 05:53:56 +0800

    test

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path(r'topics/', views.topics, name='topics'),
     #re_path(r'^topics/$', views.topics, name='topics'),
     re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    #新加主题条目
     path(r'new_topic/', views.new_topic, name='new_topic'),
+    re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm,EntryForm
 
 # Create your views here.
 
@@ -29,7 +29,7 @@ def new_topic(request):
     """添加新主题"""
     if request.method != 'POST':
         #未提交数据，创建一个新表单
-        form = TopicForm
+        form = TopicForm()
     else:
         #POST提交的数据，对数据进行处理
         form = TopicForm(request.POST)
@@ -38,3 +38,21 @@ def new_topic(request):
             return HttpResponseRedirect(reverse('learning_logs:topics'))
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """特定主题中添加新条目"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        #未提交数据，创建一个新表单
+        form = EntryForm()
+    else:
+        #POST提交的数据，对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                                args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
