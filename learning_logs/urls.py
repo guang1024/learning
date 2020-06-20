8672ab2b969d446a132ef9a5251a8722469f8c57
guang1024 <guangchuan.shi@percent.cn>
2020-06-20 17:31:28 +0800

    test

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     #错误:path(r'^$', views.index, name='index'),    #不匹配正则
     path(r'', views.index, name='index'),
     #显示所有的主题
-    #path(r'topics', views.topics, name='topics'),
-    re_path(r'^topics/$', views.topics, name='topics'),
+    path(r'topics/', views.topics, name='topics'),
+    #re_path(r'^topics/$', views.topics, name='topics'),
 ]
