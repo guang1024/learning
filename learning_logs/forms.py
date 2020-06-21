2cb9286bc1abcfe36bb33477885fa0edf3eb7068
guang1024 <guangchuan.shi@percent.cn>
2020-06-21 17:51:08 +0800

    test

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -12,5 +12,5 @@ class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = {'text'}
-        labels = {'text': ''}
+        labels = {'text': 'a'}
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
