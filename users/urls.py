92d63e0cdd117c1a4bf31bc94ffbacbe1c54c18f
guang1024 <guangchuan.shi@percent.cn>
2020-06-22 22:36:57 +0800

    test

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,12 @@
 
 from django.urls import path,re_path
 from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
 
 from . import views
 
 urlpatterns = [
     #登录界面
-    path(r'login/', login, 'users/login.html', name='login'),
+    #re_path(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+    path(r'login/', LoginView.as_view(template_name='users/login.html'), name='login')
 ]
