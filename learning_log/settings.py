e80ddd3ebaa6429ba25254f2995a72da9584af80
guang1024 <guangchuan.shi@percent.cn>
2020-06-27 07:21:55 +0800

    test

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$z626##^$(l+uw_^)til+8@npw&ugin911ixdn!+m%(&pun9pj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.14.210']
 
